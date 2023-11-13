60221841abab3d38d3eed08a82b563ac0574dfdb
James-Leigh Taylor <jamesleight@googlemail.com>
2023-11-13 18:45:58 +0000

    Added some tests to test Python Functions

--- lib/report_length.py
+++ lib/report_length.py
@@ -1,3 +1,13 @@
 def report_length(str):
     length = len(str)
     return f"This string was{length} characters long."
+
+
+def report_length_upper(str):
+    upper_length = str.upper()
+    return f"{str} in capitals is equivalent to {upper_length}"
+
+
+def report_length_lower(str):
+    lower_length = str.lower()
+    return f"{str} in lower case equals {lower_length}"
